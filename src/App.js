3d8da022b81d1f821f45375ea922943bf2598309
Stephen <smathenytech@gmail.com>
2023-11-21 14:38:10 -0600

    change 3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Firebase
+          Learn Firebase2
         </a>
       </header>
     </div>
